749d7f678349e0479a9f97b481d9559f166fdc82
michelg85 <miguelguyot85@gmail.com>
2022-09-30 08:05:47 -0300

    Subida a git

---
+++ Js/productos.js
@@ -0,0 +1,14 @@
+const productos = [
+    {id:01, tipo:"Ceviche", nombre:"Ceviche Picante", precio: 1800, foto: "./imagenes/ceviche.png" },
+    {id:02, tipo:"Eby Furai", nombre:"Eby Furai", precio: 1400, foto: "./imagenes/eby.jpg" },
+    {id:03, tipo:"Geishas", nombre:"Geishas", precio: 1000, foto: "./imagenes/gei.jpg" },
+    {id:04, tipo:"Maki", nombre:"Maki", precio: 1000, foto: "./imagenes/maki.jpg" },
+    {id:05, tipo:"Nibuta", nombre:"Nibuta", precio: 1400, foto: "./imagenes/nibuta.jpg" },
+    {id:06, tipo:"Nigiri", nombre:"Nigiris", precio: 1500, foto: "./imagenes/nigiri.jpg" },
+    {id:07, tipo: "Panko", nombre: "Panko Fresco", precio: 2700, foto: "./imagenes/panko.jpg"},
+    {id:08, tipo: "Poke", nombre: "Pokes", precio: 2000, foto: "./imagenes/poke.jpg"},
+    {id:09, tipo: "Sashimi", nombre: "Sashimis", precio: 3800, foto: "./imagenes/sashimi.jpg"},
+    {id:10, tipo: "Setas", nombre: "Setas Variedad", precio: 3800, foto: "./imagenes/setas.jpg"},
+    {id:11, tipo: "Shari", nombre: "Shari Preparado", precio: 1300, foto: "./imagenes/shari.jpg"},
+    {id:12, tipo: "Surimi", nombre: "Surimi", precio: 2200, foto: "./imagenes/suri.jpg"},
+];
